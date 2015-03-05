38b9ba14fca3fdcf37fd704babf47343db051d4f
codingeass <amandeeptheviper@gmail.com>
2015-03-05 21:49:12 +0530

    using ajax to get information

---
+++ dist/js/main.js
@@ -0,0 +1,28 @@
+function content_change(page){
+	var xmlhttp=false;
+	if (window.XMLHttpRequest)
+	{// code for IE7+, Firefox, Chrome, Opera, Safari
+		xmlhttp=new XMLHttpRequest();
+	}
+	else
+	{// code for IE6, IE5
+		xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+	}
+	if(xmlhttp)
+	{
+		
+		xmlhttp.open("GET",'dist/views/'+page+'.html');
+		xmlhttp.send();
+		xmlhttp.onreadystatechange=function()
+		{
+			if(xmlhttp.readyState==4 && xmlhttp.status==200)
+			{
+				document.getElementById('content_dir').innerHTML=xmlhttp.responseText;
+			}				
+		}				
+	}
+}
+
+window.onload =function(){
+	content_change('project');
+}
